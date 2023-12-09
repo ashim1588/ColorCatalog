3e075b878fc3ccc513b14173c3db56980e18af7e
ashim1588 <ashimmehar158@gmail.com>
2023-12-09 12:39:13 +0530

    Initial Setup

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,  Text,  View } from 'react-native';
+import Cat from './assets/jungle.jpg'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>[TODO: INSERT CAT]</Text>
     </View>
   );
 }
@@ -13,8 +12,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
+  text: {
+    textAlign: 'center',
+    fontSize: 22,
+  },
+  selectedText: {
+    backgroundColor: 'red',
+    color: 'yellow',
+  }
 });
