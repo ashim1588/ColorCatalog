be2e9f9b7aa4863128727046dfe5f608d69b0a7b
ashim1588 <ashimmehar158@gmail.com>
2023-12-12 19:03:05 +0530

    AddColor

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
-import { Alert, Dimensions, FlatList, Image, StyleSheet,  Text,  TouchableHighlight,  View } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
 import { StrictMode, useState } from 'react';
 import ColorButton from './components/ColorButton';
 import defaultColors from './data/defaultColors.json';
 import ColorForm from './components/ColorForm';
+import { generate } from 'shortid';
 
 export default function App() {
   const [backgroundColor, setBackgroundColor ] = useState("blue");
+  const [colors, setColors] = useState([]);
+  const addColor= color => {
+    const newColor = {id: generate(), color}
+    setColors([ newColor, ...colors]);
+  }
   return (
     <StrictMode>
-      <ColorForm onNewColor={newColor => Alert.alert(`TODO: add color`, newColor)}/>
+      <ColorForm onNewColor={addColor}/>
     <FlatList style={[styles.container, {backgroundColor}]}
-    data={defaultColors}
+    data={colors}
     renderItem={({ item })=> {
       return (
         <ColorButton key={item.id} color={item.color}  onPress={setBackgroundColor} />
